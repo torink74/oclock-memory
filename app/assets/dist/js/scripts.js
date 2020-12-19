f2fbfc3d3381c4c80a597d697099e3e50d318658
Adrien Delcroix <a.delcroix@acti.fr>
2020-12-19 10:51:54 +0100

    Handle game without timer

--- app/assets/dist/js/scripts.js
+++ app/assets/dist/js/scripts.js
@@ -1,30 +1,103 @@
 let cards = document.querySelectorAll('.card');
 let previousReturnedCard = null;
 
-function handleClickOnCard(event) {
-    let card = event.target;
+/**
+ * Vérifie si la partie est terminée et que le joueur a gagné
+ * @returns {boolean} True si le joueur a gagné
+ */
+function isGameWon()
+{
+    let cardsToFind = document.querySelectorAll('.hide');
 
-    let index = card.dataset.letter;
-    let letter = index.substr(0, 1);
+    /**
+     * Les cartes possédant la classe hide sont celles qui n'ont pas encore trouvé leur paire
+     */
+    if (cardsToFind.length > 0) {
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+
+function handleCardAction(card)
+{
+    /**
+     * On affiche la carte
+     */
     card.classList.remove('hide');
     if (previousReturnedCard !== null)
     {
-        if (previousReturnedCard === card.dataset.letter) {
-            // Tu viens de cliquer deux fois au meme endroit
-            console.log("C'est la même carte");
+        if (previousReturnedCard.dataset.letter === card.dataset.letter) {
+            /**
+             * La même carte a été cliqué deux fois de suite.
+             * On la retourne face cachée et on réinitialise la valeur de la carte précédente
+             */
+            previousReturnedCard = null;
+            card.classList.add('hide');
         }
         else {
-            let previousReturnedCardLetter = previousReturnedCard.substr(0, 1);
-            if (letter === previousReturnedCardLetter) {
-                console.log('Bravo');
+            /**
+             * On récupère les lettres correspondantes aux deux cartes retournées
+             * Deux cartes possédant la même lettre signifie qu'elles font parti de la même paire
+             */
+            let index = card.dataset.letter;
+            let letter = index.substr(0, 1);
+            let previousReturnedCardIndex = previousReturnedCard.dataset.letter;
+            let previousReturnedCardLetter = previousReturnedCardIndex.substr(0, 1);
+            if (letter === previousReturnedCardLetter)
+            {
+                /**
+                 * Les deux cartes retournées correspondent, on valide la paire et
+                 * on vérifie si la partie est gagnée
+                 */
+                card.classList.add('found');
+                previousReturnedCard.classList.add('found');
+                if (isGameWon()) {
+                    alert('BRAVO');
+                }
             }
             else {
-                console.log('Essaye encore');
+                /**
+                 * Les deux cartes retournées ne correspondent pas, on retourne cette carte
+                 * et la carte précédente face cachée
+                 */
+                card.classList.add('hide');
+                previousReturnedCard.classList.add('hide');
             }
+
+            /**
+             * On réinitialise la valeur de la carte précédente
+             * car nous devons jouer avec deux autres cartes
+             */
+            previousReturnedCard = null;
         }
     }
+    else {
+        /**
+         * On stocke la valeur de notre première carte retournée
+         */
+        previousReturnedCard = card;
+    }
+}
 
-    previousReturnedCard = card.dataset.letter;
+function handleClickOnCard(event) {
+    let card = event.target;
+
+    /**
+     * On vérifie si la carte cliquée ne fait pas partie d'une paire déjà trouvée
+     */
+    if (card.classList.contains('found')) {
+        /**
+         * La carte a déjà été trouvée, on ne fait rien
+         */
+    }
+    else {
+        /**
+         * On exécute les actions possibles avec cette carte
+         */
+        handleCardAction(card);
+    }
 }
 
 cards.forEach(function (card) {
