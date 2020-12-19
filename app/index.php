5a9ead71533d94ef70d65a368bd3cb5b8e594a8d
Adrien Delcroix <a.delcroix@acti.fr>
2020-12-19 19:32:15 +0100

    Remove debug functions

--- app/index.php
+++ app/index.php
@@ -5,9 +5,6 @@ use Memory\Controller\GameController;
 /* On appelle les fichiers qui ont été générés par Composer */
 require __DIR__ . '/../vendor/autoload.php';
 
-ini_set('display_errors', 'On');
-error_reporting(E_ALL);
-
 define('MEMORY_TEMPLATES_PATH', __DIR__ . '/templates/');
 define('MEMORY_ASSETS_PATH', __DIR__ . '/assets/dist/');
 
